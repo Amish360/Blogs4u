8733c72efc120443bbe2b79177c69467b7d7c377
SamBridges-Porter <sambridges.porter@koretechx.com>
2025-04-15 15:06:11 +0500

    feat(b4-5):create my blogs page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import FlipNavWrapper from "@/components/navbar";
+import { BackButton } from "@/components/backButton";
 import Footer from "@/components/footer";
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <FlipNavWrapper />
+        <BackButton />
         {children}
         <Footer />
       </body>
