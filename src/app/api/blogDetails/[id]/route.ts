ee70b4eb0deefaf78411369f26bf531d3fe1912d
Amish Toor <amishtoor@gmail.com>
2025-05-20 15:32:19 +0500

    feat(b4-22):Create update and delete blogs api

--- src/app/api/blogDetails/[id]/route.ts
+++ src/app/api/blogDetails/[id]/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
 export async function GET(
@@ -35,5 +34,57 @@ export async function GET(
       { error: "Internal Server Error" },
       { status: 500 }
     );
+  }
+}
+
+export async function DELETE(
+  _req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const blogId = parseInt((await params).id);
+    if (isNaN(blogId)) {
+      return NextResponse.json({ error: "Invalid blog ID" }, { status: 400 });
+    }
+
+    const blog = await prisma.blog.delete({
+      where: { id: blogId },
+    });
+
+    if (!blog) {
+      return NextResponse.json({ error: "Blog not found" }, { status: 400 });
+    }
+
+    return NextResponse.json({ message: "Blog deleted successfully" });
+  } catch {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const blogId = parseInt((await params).id);
+    if (isNaN(blogId)) {
+      return NextResponse.json({ error: "Invalid blog ID" }, { status: 400 });
+    }
+
+    const body = await req.json();
+    const { title, content, categoryId } = body;
+
+    const updatedBlog = await prisma.blog.update({
+      where: { id: blogId },
+      data: {
+        title,
+        content,
+        categoryId,
+      },
+    });
+
+    return NextResponse.json(updatedBlog);
+  } catch {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
